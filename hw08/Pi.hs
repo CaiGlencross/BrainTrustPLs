9b2041b10e685ec5c73d141c910c047c1027a54b
Thomas Thornton <thomaswilliamthornton@gmail.com>
2017-11-30 20:34:22 -0800

    progress on evaluator of Pi

--- hw08/Pi.hs
+++ hw08/Pi.hs
@@ -177,7 +177,35 @@ evalExp env (ETup es) = VTup (evalExps env es)
     evalExps env (e:es) = evalExp env e : evalExps env es
 
 run :: Env -> Pi -> IO ()
-run = undefined
+run env Nil = IO ()
+run env (p1 :|: p2) = Concurrent.parallel (run env p1) (run env p2)
+run env (New name typ pi) = 
+    do
+    chan <- newChan
+    let env' = Map.insert name (VChan chan) env
+    return $ run env' pi
+run env (Out name exp) =
+    case lookup name env of
+        (Right (VChan chan)) -> writeChan chan exp 
+        (Right _) -> type_error "sending to nonchannel"
+        (Left err)   -> error "output to nonexistent channel"
+run env (Inp name pat pi) =
+    do
+    let chan = case Map.lookup name env of
+               (Just chan) -> chan
+               _   -> error "input from nonexistent channel"
+    val <- readChan chan 
+    let env' = evalPat env pat val
+    run env' pi
+run env (RepInp name pat pi) =
+    do
+    let chan = case Map.lookup name env of
+               (Just (VChan chan)) -> chan
+               (Just _) -> type_error "sending to nonchannel"
+               _   -> error "input from nonexistent channel"
+    val <- readChan chan 
+    let env' = evalPat env pat val
+    run env' ((RepInp name pat pi) :|: pi)
 
 start :: Pi -> IO ()
 start p = run Map.empty p
